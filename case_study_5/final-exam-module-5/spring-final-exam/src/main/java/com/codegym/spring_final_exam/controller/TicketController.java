2455f335bb3b4d6b1e33fc4f651efae671385f70
C0222G1-NguyenQuangPhuc <phucprokoten@gmail.com>
2022-07-29 13:05:16 +0700

    update final-exam-module-5 ngay 29/07/2022

---
+++ case_study_5/final-exam-module-5/spring-final-exam/src/main/java/com/codegym/spring_final_exam/controller/TicketController.java
@@ -0,0 +1,59 @@
+package com.codegym.spring_final_exam.controller;
+
+import com.codegym.spring_final_exam.model.Bus;
+import com.codegym.spring_final_exam.model.Ticket;
+import com.codegym.spring_final_exam.service.IBusService;
+import com.codegym.spring_final_exam.service.ITicketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class TicketController {
+    @Autowired
+    private ITicketService iTicketService;
+
+    @Autowired
+    private IBusService iBusService;
+
+    @GetMapping("/list-ticket")
+    public ResponseEntity<?> showListTicket(@RequestParam(name = "page", defaultValue = "0") int page) {
+        Sort sort = Sort.by("ticket_id").ascending();
+        Page<Ticket> tickets = iTicketService.findAllTicket(PageRequest.of(page, 100, sort));
+        return new ResponseEntity<>(tickets, HttpStatus.OK);
+    }
+
+    @GetMapping("/list-bus")
+    public ResponseEntity<?> showListBus() {
+        List<Bus> busList = iBusService.findAllBus();
+        return new ResponseEntity<>(busList, HttpStatus.OK);
+    }
+
+    @PostMapping("/list-ticket/create")
+    public ResponseEntity<?> createTicket(@Valid @RequestBody Ticket ticket) {
+        iTicketService.save(ticket);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/order")
+    public ResponseEntity<?> orderTicket(@RequestBody Ticket ticket ){
+        iTicketService.save(ticket);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/search-destination/{txt}")
+    public ResponseEntity<?> searchByDestination(@RequestParam(name = "page", defaultValue = "0") int page, @PathVariable String txt) {
+        Sort sort = Sort.by("ticket_id").ascending();
+        Page<Ticket> tickets = iTicketService.searchByDestination(PageRequest.of(page, 100, sort), txt);
+        return new ResponseEntity<>(tickets, HttpStatus.OK);
+    }
+}
